4535d481bfe2004124007ae62f8ac675afb70685
unknown <jamesdoth@gmail.com>
2022-08-15 16:35:02 -0700

    shot graphics

--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::{
     frame::{Drawable, Frame},
     shot::Shot,
@@ -15,6 +17,7 @@ impl Player {
         Self {
             x: NUM_COLS / 2,
             y: NUM_ROWS - 1,
+            shots: Vec::new(),
         }
     }
     pub fn move_left(&mut self) {
@@ -27,10 +30,27 @@ impl Player {
             self.x += 1;
         }
     }
+    pub fn shoot(&mut self) -> bool {
+        if self.shots.len() < 2 {
+            self.shots.push(Shot::new(self.x, self.y - 1));
+            true
+        } else {
+            false
+        }
+    }
+    pub fn update(&mut self, delta: Duration) {
+        for shot in self.shots.iter_mut() {
+            shot.update(delta);
+        }
+        self.shots.retain(|shot| !shot.dead());
+    }
 }
 
 impl Drawable for Player {
     fn draw(&self, frame: &mut Frame) {
         frame[self.x][self.y] = "A";
+        for shot in self.shots.iter() {
+            shot.draw(frame);
+        }
     }
 }
